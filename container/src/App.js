dc7b7d1022bb4a2c9907398e45ab1a27463c9352
Prafull37 <prafullsingh37@gmail.com>
2021-03-13 22:35:43 +0530

    InitialCommit for Dep

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./Composer/MarketingApp";
+
+
+const App=()=>{
+    return <div>
+        <h1>Contaier...</h1>
+        <hr/>
+        <MarketingApp/>
+    </div>
+}
+
+export default App;
